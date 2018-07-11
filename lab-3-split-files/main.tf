542beeacb64b5925ded7f1caa0eda444a58e5ce7
Matthias Buchner <matt@flux7.com>
2018-07-11 11:53:04 -0500

    Update main.tf

--- lab-3-split-files/main.tf
+++ lab-3-split-files/main.tf
@@ -2,5 +2,5 @@ resource "aws_instance" "rhel_instance" {
   ami           = "${lookup(var.amis, var.aws_region)}"
   instance_type = "${var.instance_type}"
   subnet_id     = "${var.public_subnet_id}"
-  tags = "${var.tags}"
+  tags          = "${var.tags}"
 }
