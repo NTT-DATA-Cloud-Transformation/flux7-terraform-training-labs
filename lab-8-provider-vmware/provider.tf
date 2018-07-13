2dc6835c1495e3ceec4a0ef6fc49350b2f2ab0b7
Cloud User <ec2-user@ip-10-192-10-61.ap-southeast-1.compute.internal>
2018-07-13 05:27:27 +0000

    who needs root credentials anyway?

--- lab-8-provider-vmware/provider.tf
+++ lab-8-provider-vmware/provider.tf
@@ -1,6 +1,6 @@
 provider "vsphere" {
   user           = "root"
-  password       = "L/xbi'FvCKLc"
+  password       = "changeme"
   vsphere_server = "158.69.124.88"
 
   # If you have a self-signed cert
